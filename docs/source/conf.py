15cb2f2e82a9bcb27a724950c872e48559b8e82b
JSim011235 <75387248+JSim011235@users.noreply.github.com>
2026-02-12 23:44:18 -0600

    Change navigation header background color

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': '#500000',\
+    'style_nav_header_background': '#f3d127',\
     'collapse_navigation': False,
     'sticky_navigation': True,
 }
